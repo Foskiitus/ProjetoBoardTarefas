657b5f3710b42d6736c909e2a286d45deeb86969
Foskiitus <foskiitus@gmail.com>
2024-05-12 21:01:16 +0200

    secao 5 aula 39/40 criando a dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,11 @@
 import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
+import { Textarea } from "@/components/textarea";
 
 import { getSession } from "next-auth/react";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 export default function Dashboard() {
   return (
@@ -11,7 +14,43 @@ export default function Dashboard() {
         <title>Meu painel de tarefas</title>
       </Head>
 
-      <h1>Pagina Painel</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <div className={styles.title}>Qual sua tarefa?</div>
+
+            <form action="">
+              <Textarea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label> Deixar tarefa publica?</label>
+              </div>
+              <button className={styles.button} type="submit">
+                Registar
+              </button>
+            </form>
+          </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo show demais!</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
+        </section>
+      </main>
     </div>
   );
 }
