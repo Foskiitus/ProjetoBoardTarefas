33980e93ea97bbc71b16ea144e299ca24418b6eb
Foskiitus <foskiitus@gmail.com>
2024-05-18 18:14:18 +0200

    secao 5 aula 51 criando pagina inicial estatica gerada a cada 60 segundos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,16 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 import heroImg from "@/../public/assets/hero.png";
+import { GetStaticProps } from "next";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/services/firebaseConnection";
 
-export default function Home() {
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -25,10 +33,10 @@ export default function Home() {
 
           <div className={styles.infoContent}>
             <section className={styles.box}>
-              <span>+12 posts</span>
+              <span>+{posts} posts</span>
             </section>
             <section className={styles.box}>
-              <span>+102 comentários</span>
+              <span>+{comments} comentários</span>
             </section>
           </div>
         </div>
@@ -36,3 +44,20 @@ export default function Home() {
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  //Buscar os numeros e enviar para o cliente
+
+  const commentRef = collection(db, "comments");
+  const postRef = collection(db, "tasks");
+
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0,
+    },
+    revalidate: 60, // seria revalidada a cada 60segundos
+  };
+};
